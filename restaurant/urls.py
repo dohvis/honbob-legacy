bb58d3a892dbde067e857cb3ba419e12cc1c2199
nerogit <nero.union12@gmail.com>
2015-09-30 00:15:28 +0900

    applied ajax

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,6 +3,6 @@ from django.views.generic import TemplateView
 from django.contrib.auth.views import logout
 from restaurant import views
 urlpatterns = [
-    url(r'^list/(?P<min_x>\d*\.\d*)/(?P<min_y>\d*\.\d*)/(?P<max_x>\d*\.\d*)/(?P<max_y>\d*\.\d*)/$',
+    url(r'^list/$',
         views.serializer),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 from restaurant.models import Restaurant
 from json import dumps
 
-def serializer(request,min_x,min_y,max_x,max_y):
+def serializer(request):
+    min_x = request.GET['min_x']
+    min_y = request.GET['min_y']
+    max_x = request.GET['max_x']
+    max_y = request.GET['max_y']
+
     x = list(Restaurant.objects.filter(x__range=(min_x,max_x)))
     y = list(Restaurant.objects.filter(y__range=(min_y,max_y)))
     res = []
